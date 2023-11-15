8048755f2d8e2ebfa3077e17f1f49ec4e5755a66
LucasdeAl <lucasde@alu.ufc.br>
2023-11-15 15:56:01 -0300

    Dilatação com Elemento estruturante retangular pronto

--- main.cpp
+++ main.cpp
@@ -35,13 +35,59 @@ Mat ErosionRetangle(Mat image,std::pair<int, int> msize){
                     }
                 }
             }
-            result !=0 ? pixelAppliedPtr[i*image.cols + j] = round(result * 255.0): pixelAppliedPtr[i*image.cols + j] = 0;
+            result !=0 ? pixelAppliedPtr[i*image.cols + j] = 255: pixelAppliedPtr[i*image.cols + j] = 0;
             }
         }
     
 
 
-    String windowName = "Imagem com kernel customizado";
+    String windowName = "Erosão com kernel retangular";
+
+    namedWindow(windowName);
+
+    imshow(windowName, applied);
+
+    waitKey(0);
+
+    destroyWindow(windowName);
+
+    return applied;
+}
+
+Mat DilatationRetangle(Mat image,std::pair<int, int> msize){
+    int X = static_cast<int>(image.rows);
+    int Y = static_cast<int>(image.cols);
+    Mat applied = Mat(X, Y, CV_8UC1, Scalar::all(0));
+    int lin = msize.first, col = msize.second;
+    double size = lin*col;
+    std::vector<double> kernel(size,1); 
+    uint8_t* pixelImagePtr;
+    uint8_t* pixelAppliedPtr;
+    pixelImagePtr = (uint8_t*)image.data;
+    pixelAppliedPtr = (uint8_t*)applied.data;
+    int reflin = lin/2, refcol = col/2;
+    for(int i = 0; i < image.rows; i++)
+    {
+        for(int j = 0; j < image.cols; j++)
+        {
+            int result = 0;
+            for(int l = 0; l < lin; l++){
+                for(int c = 0; c < col; c++){
+                    int thisline = i + (l - reflin), thiscol = j + (c - refcol);
+                    if(thisline >= 0 and thisline < image.rows){
+                        if(thiscol >= 0 and thiscol < image.cols and ((int)kernel[l*col + c])==1){
+                            result += (int)((pixelImagePtr[thisline*image.cols + thiscol]/255.0) * kernel[l*col + c]);
+                        }
+                    }
+                }
+            }
+            result !=0 ? pixelAppliedPtr[i*image.cols + j] = 255: pixelAppliedPtr[i*image.cols + j] = 0;
+            }
+        }
+    
+
+
+    String windowName = "Dilatação com kernel retangular";
 
     namedWindow(windowName);
 
@@ -71,7 +117,7 @@ int main(int argc, char** argv)
         return -1;
     }
    
-    Mat equalized = ErosionRetangle(image,{45,45});
+    Mat equalized = DilatationRetangle(image,{15,15});
     //imshow("Window Name", equalized);
     
     //waitKey(0);
